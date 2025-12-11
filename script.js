0b98b447454a8638bfb80020464405bdc927b85d
Pavitra <pavitrapattanashetty06@gmaill.com>
2025-12-11 09:29:41 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function addstudent() {
+    let input = document.querySelector('#input');
+    let name = input.value.trim();
+
+    let li =document.createElement('li');//creating a list element for veiwing the student list
+    let span = document.createElement('span');
+    span.innerText=name;
+
+    let del =document.createElement('button');
+    del.innerText='Del';
+
+    del.onclick = function(){
+        li.remove();
+    }
+
+    li.appendChild(span);
+    li.appendChild(del);
+   
+
+    document.querySelector('#list').appendChild(li);
+
+    input.value ='';
+    let edit=document.createElement('button');
+    edit.innerText='edit';
+    edit.onclick=function(){
+        li.remove();
+    
+    }
+   
+    li.appendChild(edit);
+}
